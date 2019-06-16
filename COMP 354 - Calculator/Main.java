90dc2706028d7298e9c5388705da14297bd25d87
danon360 <fakhrdany@gmail.com>
2019-06-16 04:16:43 -0400

    updated main.java

--- COMP 354 - Calculator/Main.java
+++ COMP 354 - Calculator/Main.java
@@ -13,133 +13,133 @@ public class Main
 
 		//keeps looping until user types exit
 		while(true){
-		System.out.println("Please input the name of the function followed by the parameters (separated by spaces): (you can type 'man [FunctionName] to get more information about the function)" +
-				"\n\t- Addition (...)				-e^ (1)"+
-				"\n\t- Subtraction (...)             -power (2)"+
-				"\n\t- Multiplication (...)						"+
-				"\n\t- Division (..)			  				"+
-				"\n\t- root (2)										"+
-				"\n\t- factorial (1)								"+
-				"\n\t- ln (1) 										"+
-				"\n\t- exit (0)"
+			System.out.println("Please input the number for the function:   (you can type 'man [FunctionName] to get a manual of the function)" +
+					"\n\t- Addition (...)				-e^ (1)"+
+					"\n\t- Subtraction (...)             -power (2)"+
+					"\n\t- Multiplication (...)						"+
+					"\n\t- Division (..)			  				"+
+					"\n\t- root (2)										"+
+					"\n\t- factorial (1)								"+
+					"\n\t- ln (1) 										"+
+					"\n\t- exit (0)"
 
-		);
-		// obtaining the inputed string and splitting it at " "
-		splited = input.nextLine().split(" ");
+			);
+			// obtaining the inputed string and splitting it at " "
+			splited = input.nextLine().split(" ");
 
-		//converting the function name to lower case
-		function = splited[0].toLowerCase();
+			//converting the function name to lower case
+			function = splited[0].toLowerCase();
 
-		arguments= new double[splited.length-1];
-
-
-		for (int i = 1; i <=arguments.length ; i++) {
-			try {
-				arguments[i-1]=Double.parseDouble(splited[i]);
-			}catch (NumberFormatException e){
-				manStr=splited[i];
-			}
-		}
+			arguments= new double[splited.length-1];
 
 
-		switch (function) {
-			case "addition": {
-				System.out.println(MathFunctions.add(arguments));
-				break;
-			}
-			case "subtraction": {
-				System.out.println(MathFunctions.subtract(arguments));
-				break;
-			}
-			case "multiplication": {
-				System.out.println(MathFunctions.multiply(arguments));
-				break;
-			}
-			case "division": {
-				System.out.println(MathFunctions.divide(arguments));
-				break;
-
-			}
-			case "root": {
-				if (arguments[1]!=(int)arguments[1]){
-					System.out.println("The root has to be an integer");
-					break;
-				}else {
-					System.out.println(MathFunctions.nroot(arguments[0], (int) arguments[1]));
+			for (int i = 1; i <=arguments.length ; i++) {
+				try {
+					arguments[i-1]=Double.parseDouble(splited[i]);
+				}catch (NumberFormatException e){
+					manStr=splited[i];
 				}
 			}
-			case "factorial": {
-				if (arguments[0]!=(int)arguments[0]) {
-					System.out.println("Math error: argument has to be an integer");
-					break;
-				}else {
-					System.out.println(MathFunctions.factorial((int) arguments[0]));
+
+
+			switch (function) {
+				case "addition": {
+					System.out.println(MathFunctions.add(arguments));
 					break;
 				}
-			}
-			case "ln":{
-				System.out.println(Transcendentals.ln(arguments[0]));
-				break;
-			}
-			case "e^":{
-				System.out.println(Transcendentals.e_to_x(arguments[0]));
-				break;
-			}
-			case "power":
-				System.out.println(Transcendentals.power(arguments[0],arguments[1]));
-				break;
-			case "man":{
-				switch (manStr){
-					case "addition":{
-						System.out.println(manual[0]);
-						break;
-					}
-					case "subtraction":{
-						System.out.println(manual[1]);
-						break;
-					}
-					case "multiplication":{
-						System.out.println(manual[2]);
-						break;
-					}
-					case "division":{
-						System.out.println(manual[3]);
-						break;
-					}
-					case "root":{
-						System.out.println(manual[4]);
-						break;
-					}
-					case "factorial":{
-						System.out.println(manual[5]);
-						break;
-					}
-					case "ln":{
-						System.out.println(manual[6]);
-						break;
-					}
-					case "e^":{
-						System.out.println(manual[7]);
-						break;
-					}
-					case "power":{
-						System.out.println(manual[8]);
-						break;
-					}
-					default:{
-						System.out.println("Could not find the function");
-					}
+				case "subtraction": {
+					System.out.println(MathFunctions.subtract(arguments));
+					break;
+				}
+				case "multiplication": {
+					System.out.println(MathFunctions.multiply(arguments));
+					break;
+				}
+				case "division": {
+					System.out.println(MathFunctions.divide(arguments));
+					break;
 
 				}
-				break;
+				case "root": {
+					if (arguments[1]!=(int)arguments[1]){
+						System.out.println("The root has to be an integer");
+						break;
+					}else {
+						System.out.println(MathFunctions.nroot(arguments[0], (int) arguments[1]));
+					}
+				}
+				case "factorial": {
+					if (arguments[0]!=(int)arguments[0]) {
+						System.out.println("Math error: argument has to be an integer");
+						break;
+					}else {
+						System.out.println(MathFunctions.factorial((int) arguments[0]));
+						break;
+					}
+				}
+				case "ln":{
+					System.out.println(Transcendentals.ln(arguments[0]));
+					break;
+				}
+				case "e^":{
+					System.out.println(Transcendentals.e_to_x(arguments[0]));
+					break;
+				}
+				case "power":
+					System.out.println(Transcendentals.power(arguments[0],arguments[1]));
+					break;
+				case "man":{
+					switch (manStr){
+						case "addition":{
+							System.out.println(manual[0]);
+							break;
+						}
+						case "subtraction":{
+							System.out.println(manual[1]);
+							break;
+						}
+						case "multiplication":{
+							System.out.println(manual[2]);
+							break;
+						}
+						case "division":{
+							System.out.println(manual[3]);
+							break;
+						}
+						case "root":{
+							System.out.println(manual[4]);
+							break;
+						}
+						case "factorial":{
+							System.out.println(manual[5]);
+							break;
+						}
+						case "ln":{
+							System.out.println(manual[6]);
+							break;
+						}
+						case "e^":{
+							System.out.println(manual[7]);
+							break;
+						}
+						case "power":{
+							System.out.println(manual[8]);
+							break;
+						}
+						default:{
+							System.out.println("Could not find the function");
+						}
+
+					}
+					break;
+				}
+				case "exit": {
+					System.exit(0);
+				}
+				default:{
+					System.out.println("Could not find the function");
+				}
 			}
-			case "exit": {
-				System.exit(0);
-			}
-			default:{
-				System.out.println("Could not find the function");
-			}
-		}
 		}
 
 
