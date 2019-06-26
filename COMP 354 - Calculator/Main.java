bb199c0d7a0d7fc0384618d66ca37f1728a1f6df
danon360 <fakhrdany@gmail.com>
2019-06-25 20:02:51 -0400

    added some features in main. numbers are no onger printed in scientific notation and created a printManual function. )

--- COMP 354 - Calculator/Main.java
+++ COMP 354 - Calculator/Main.java
@@ -10,18 +10,19 @@ public class Main
 		String[] splited;	//raw input string seperated by " "
 		String function;	//String variable for the function name
 		double[] arguments; //String array of arguments given after the function name
+		String decimallength=".15"; //number of desimals to display
 
 		//keeps looping until user types exit
 		while(true){
-			System.out.println("Please input the number for the function:   (you can type 'man [FunctionName] to get a manual of the function)" +
-					"\n\t- Addition (...)				-e^ (1)"+
-					"\n\t- Subtraction (...)             -power (2)"+
-					"\n\t- Multiplication (...)						"+
-					"\n\t- Division (..)			  				"+
-					"\n\t- root (2)										"+
-					"\n\t- factorial (1)								"+
-					"\n\t- ln (1) 										"+
-					"\n\t- exit (0)"
+			System.out.println("\nPlease input the function name, followed by the required parameters, or type in 'man [FunctionName]' for function manual\n" +
+					"\n\t1- Addition (...)				9- e^ (1)"+
+					"\n\t2- Subtraction (...)            10- power (2)"+
+					"\n\t3- Multiplication (...)			"+
+					"\n\t4- Division (..)			  				"+
+					"\n\t5- root (2)										"+
+					"\n\t6- factorial (1)								"+
+					"\n\t7- ln (1) 										"+
+					"\n\t8- exit (0)"
 
 			);
 			// obtaining the inputed string and splitting it at " "
@@ -44,19 +45,19 @@ public class Main
 
 			switch (function) {
 				case "addition": {
-					System.out.println(MathFunctions.add(arguments));
+					System.out.printf("%"+decimallength+"f \n",MathFunctions.add(arguments));
 					break;
 				}
 				case "subtraction": {
-					System.out.println(MathFunctions.subtract(arguments));
+					System.out.printf("%"+decimallength+"f \n",MathFunctions.subtract(arguments));
 					break;
 				}
 				case "multiplication": {
-					System.out.println(MathFunctions.multiply(arguments));
+					System.out.printf("%"+decimallength+"f \n",MathFunctions.multiply(arguments));
 					break;
 				}
 				case "division": {
-					System.out.println(MathFunctions.divide(arguments));
+					System.out.printf("%"+decimallength+"f \n",MathFunctions.divide(arguments));
 					break;
 
 				}
@@ -65,7 +66,7 @@ public class Main
 						System.out.println("The root has to be an integer");
 						break;
 					}else {
-						System.out.println(MathFunctions.nroot(arguments[0], (int) arguments[1]));
+						System.out.printf("%"+decimallength+"f \n",MathFunctions.nroot(arguments[0], (int) arguments[1]));
 					}
 				}
 				case "factorial": {
@@ -73,64 +74,28 @@ public class Main
 						System.out.println("Math error: argument has to be an integer");
 						break;
 					}else {
-						System.out.println(MathFunctions.factorial((int) arguments[0]));
+						System.out.printf("%"+decimallength+"f \n",MathFunctions.factorial((int) arguments[0]));
 						break;
 					}
 				}
 				case "ln":{
-					System.out.println(Transcendentals.ln(arguments[0]));
+					System.out.printf("%"+decimallength+"f \n",Transcendentals.ln(arguments[0]));
 					break;
 				}
 				case "e^":{
-					System.out.println(Transcendentals.e_to_x(arguments[0]));
+					System.out.printf("%"+decimallength+"f \n",Transcendentals.e_to_x(arguments[0]));
 					break;
 				}
-				case "power":
-					System.out.println(Transcendentals.power(arguments[0],arguments[1]));
-					break;
-				case "man":{
-					switch (manStr){
-						case "addition":{
-							System.out.println(manual[0]);
-							break;
-						}
-						case "subtraction":{
-							System.out.println(manual[1]);
-							break;
-						}
-						case "multiplication":{
-							System.out.println(manual[2]);
-							break;
-						}
-						case "division":{
-							System.out.println(manual[3]);
-							break;
-						}
-						case "root":{
-							System.out.println(manual[4]);
-							break;
-						}
-						case "factorial":{
-							System.out.println(manual[5]);
-							break;
-						}
-						case "ln":{
-							System.out.println(manual[6]);
-							break;
-						}
-						case "e^":{
-							System.out.println(manual[7]);
-							break;
-						}
-						case "power":{
-							System.out.println(manual[8]);
-							break;
-						}
-						default:{
-							System.out.println("Could not find the function");
-						}
-
+				case "power":{
+					try {
+						System.out.printf("%" + decimallength + "f \n", Transcendentals.power(arguments[0], arguments[1]));
+					} catch (ImaginaryNumberException e) {
+						System.err.println("Math Error : Imaginary Number ");
 					}
+					break;
+				}
+				case "man":{
+					printManual(manStr);
 					break;
 				}
 				case "exit": {
@@ -145,52 +110,98 @@ public class Main
 
 
 	}
-	public static String[] manual = {"-Addition (...)\n" +
-			"  SYNTAX: addition [Double ...]\n" +
-			"  PARAMETER LIMIT: infinit  \n" +
-			"  RETURN TYPE: Double \n" +
-			"\n","-Subtraction (...)\n" +
-			"  SYNTAX: subtraction [Double ...]\n" +
-			"  PARAMETER LIMIT: infinit  \n" +
-			"  RETURN TYPE: Double  \n" +
-			"\n","-Multiplication (...)\n" +
-			"  SYNTAX: multiplication [Double ...]\n" +
-			"  PARAMETER LIMIT: infinit \n" +
-			"  DESCRIPTION: Multiplies the all the numbers in the parameters list \n" +
-			"  RETURN TYPE: Double \n" +
-			"\n","-Division (...)\n" +
-			"  SYNTAX: division [Double ...]\n" +
-			"  PARAMETER LIMIT: infinit\n" +
-			"  RETURN TYPE: Double \n" +
-			"  DESCRUPTION: Divides the first parameters by the 2nd. The answers of \n" +
-			"               that division is divided by the 3rd, which is then \n" +
-			"\t       devided by the 4th...\n" +
-			"  RETURN TYPE: Double \n" +
-			"\n","-root (2)\n" +
-			"  SYNTAX: root [Double] [Integer]\n" +
-			"  PARAMETER LIMIT: 2\n" +
-			"  DESCRIPTION: Calculates the nth root of the 1st parameter, where n is\n" +
-			"               the 2nd parameter\n" +
-			"  RETURN TYPE: Double \n" +
-			"\n","-factorial (1)\n" +
-			"  SYNTAX: factorial [Long/Integer]\n" +
-			"  PARAMETER LIMIT: 1\n" +
-			"  DESCRIPTION: Calculates factorial of the 1st parameter\n" +
-			"  RETURN TYPE: Long \n" +
-			"\n","-ln (1)\n" +
-			"  SYNTAX: ln [Double]\n" +
-			"  PARAMETER LIMIT: 1\n" +
-			"  DESCRIPTION: Calculates natural logarithm of the 1st parameter\n" +
-			"  RETURN TYPE: Double \n" +
-			"\n","-e^ (1)\n" +
-			"  SYNTAX: e^ [Double]\n" +
-			"  PARAMETER LIMIT: 1\n" +
-			"  DESCRIPTION: Calculates eulers number to the power of 1st parameter\n" +
-			"  RETURN TYPE: Double","-power (2)\n" +
-			"  SYNTAX: e^ [Double, Double]\n" +
-			"  PARAMETER LIMIT: 2\n" +
-			"  DESCRIPTION: Calculates parameter 1 to the power of parameter 2\n" +
-			"  RETURN TYPE: Double "};
+	public static void printManual(String s){
+
+		String[] manual = {"-Addition (...)\n" +
+				"  SYNTAX: addition [Double ...]\n" +
+				"  PARAMETER LIMIT: infinit  \n" +
+				"  RETURN TYPE: Double \n" +
+				"\n","-Subtraction (...)\n" +
+				"  SYNTAX: subtraction [Double ...]\n" +
+				"  PARAMETER LIMIT: infinit  \n" +
+				"  RETURN TYPE: Double  \n" +
+				"\n","-Multiplication (...)\n" +
+				"  SYNTAX: multiplication [Double ...]\n" +
+				"  PARAMETER LIMIT: infinit \n" +
+				"  DESCRIPTION: Multiplies the all the numbers in the parameters list \n" +
+				"  RETURN TYPE: Double \n" +
+				"\n","-Division (...)\n" +
+				"  SYNTAX: division [Double ...]\n" +
+				"  PARAMETER LIMIT: infinit\n" +
+				"  RETURN TYPE: Double \n" +
+				"  DESCRUPTION: Divides the first parameters by the 2nd. The answers of \n" +
+				"               that division is divided by the 3rd, which is then \n" +
+				"\t       devided by the 4th...\n" +
+				"  RETURN TYPE: Double \n" +
+				"\n","-root (2)\n" +
+				"  SYNTAX: root [Double] [Integer]\n" +
+				"  PARAMETER LIMIT: 2\n" +
+				"  DESCRIPTION: Calculates the nth root of the 1st parameter, where n is\n" +
+				"               the 2nd parameter\n" +
+				"  RETURN TYPE: Double \n" +
+				"\n","-factorial (1)\n" +
+				"  SYNTAX: factorial [Long/Integer]\n" +
+				"  PARAMETER LIMIT: 1\n" +
+				"  DESCRIPTION: Calculates factorial of the 1st parameter\n" +
+				"  RETURN TYPE: Long \n" +
+				"\n","-ln (1)\n" +
+				"  SYNTAX: ln [Double]\n" +
+				"  PARAMETER LIMIT: 1\n" +
+				"  DESCRIPTION: Calculates natural logarithm of the 1st parameter\n" +
+				"  RETURN TYPE: Double \n" +
+				"\n","-e^ (1)\n" +
+				"  SYNTAX: e^ [Double]\n" +
+				"  PARAMETER LIMIT: 1\n" +
+				"  DESCRIPTION: Calculates eulers number to the power of 1st parameter\n" +
+				"  RETURN TYPE: Double","-power (2)\n" +
+				"  SYNTAX: x^ [Double, Double]\n" +
+				"  PARAMETER LIMIT: 2\n" +
+				"  DESCRIPTION: Calculates parameter 1 to the power of parameter 2\n" +
+				"  RETURN TYPE: Double "};
+
+		switch (s){
+			case "addition":{
+				System.out.println(manual[0]);
+				break;
+			}
+			case "subtraction":{
+				System.out.println(manual[1]);
+				break;
+			}
+			case "multiplication":{
+				System.out.println(manual[2]);
+				break;
+			}
+			case "division":{
+				System.out.println(manual[3]);
+				break;
+			}
+			case "root":{
+				System.out.println(manual[4]);
+				break;
+			}
+			case "factorial":{
+				System.out.println(manual[5]);
+				break;
+			}
+			case "ln":{
+				System.out.println(manual[6]);
+				break;
+			}
+			case "e^":{
+				System.out.println(manual[7]);
+				break;
+			}
+			case "power":{
+				System.out.println(manual[8]);
+				break;
+			}
+			default:{
+				System.out.println("Could not find the function");
+			}
+
+		}
+	}
 
 }
 
