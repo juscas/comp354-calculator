ca00d730f6e5586de9dd0d55b10c583481688c24
Daniel Thibault-Shea <daniel.thibaultshea@gmail.com>
2019-06-30 12:56:48 -0400

    sin, cos, tan, sec, csc, cotan to 10-8. SHTFException
    the above trig functions work great! Consider them done!
    SHTFException is thrown when something happens and we have no clue (ie. something that should never ever happen)

--- COMP 354 - Calculator/MathFunctions.java
+++ COMP 354 - Calculator/MathFunctions.java
@@ -321,7 +321,7 @@ public class MathFunctions
 		return result;
 	}
 	
-		/**
+	/**
 	 * This gives the value of cos(x) where x is in radians.
 	 * @param radians: double
 	 * @return cos(radians): double
@@ -332,39 +332,166 @@ public class MathFunctions
 		int accuracy = 6; // how many terms to calculate to (max 6 or else imprecise)
 		int posNeg = -1; // used to alternate between +/- in the series.
 		
-		radians = bringRadiansAroundZero(radians);
+		// get the angles down to a positive multiple of 2Pi since cos is periodic around 2Pi
+		radians = mapRadiansToBetween0and2Pi(radians);
+		
+		// given that the angle is now a multiple of 2Pi, get which quadrant of the cos curve its on
+		int quadrantOf2Pi = whatQuadrantOf2Pi(radians);
+		
+		// These are the mappings from the first quadrant to the appropriate quadrant.
+		switch(quadrantOf2Pi) {
+			case 1 :
+				// do nothing
+				break;
+			case 2 :
+				radians = (MathFunctions.PI - radians);
+				break;
+			case 3 :
+				radians = (radians - MathFunctions.PI);
+				break;
+			case 4 :
+				radians = ((MathFunctions.PI * 2) - radians);
+				break;
+			default :
+				break;
+		}
 		
 		// This is the Taylor expansion of cos(x)
 		for(int i = 1; i <= accuracy; ++i) {
-			result += posNeg * (MathFunctions.intPower(radians, 2*i) / MathFunctions.factorial(2*i));
+			result += posNeg * (MathFunctions.intPower(radians, 2*i) / 
+					MathFunctions.factorial(2*i));
 			posNeg = -posNeg; // flip sign for the next term
 		}
+		
+		if(quadrantOf2Pi == 2 || quadrantOf2Pi == 3 ) // since cos(x + pi) = -cos(x) we change signs
+			result = -result;
 		
 		return result;
 	}
 	
 	
 	/**
-	 * This is a helper functions. Some taylor expansions are only precise for values arount 0. This
-	 * method aims to bring radian angles down by multiples of 2Pi to get the values that is closest
-	 * to 0. (useful for ex. sin(x), cos(x).
+	 * This give secant(x) where x is in radians.
 	 * @param radians: double
-	 * @return radians around 0: double.
+	 * @return secant(x) : double
 	 */
-	private static double bringRadiansAroundZero(double radians) {
-		
-		radians %= 2 * MathFunctions.PI;
-		
-		double alternateRadians = 0.0;
-		
-		if(radians > 0)
-			alternateRadians = radians - (2 * MathFunctions.PI);
-		else
-			alternateRadians = radians + (2 * MathFunctions.PI);
-		
-		if(MathFunctions.abs(alternateRadians) < MathFunctions.abs(radians))
-			radians = alternateRadians; // use the alternate instead.
-		
-		return radians;
+	public static double sec(double radians) {
+		return 1 / cos(radians);
 	}
+	
+	/**
+	 * This gives the value of cos(x) where x is in radians.
+	 * @param radians: double
+	 * @return cos(radians): double
+	 */
+	public static double sin(double radians) {
+		
+		// get the angles down to a positive multiple of 2Pi since cos is periodic around 2Pi
+		radians = mapRadiansToBetween0and2Pi(radians);
+		
+		double result = 0; 
+		int accuracy = 6; // how many terms to calculate to (max 6 or else imprecise)
+		int posNeg = -1; // used to alternate between +/- in the series.
+		
+		// The 2nd and 3rd quadrants of the 0 to 2Pi curve lie under the x-axis -> flip sign
+		int quadrantOf2Pi = whatQuadrantOf2Pi(radians);
+		
+		// These are the mappings from the first quadrant to the appropriate quadrant.
+		switch(quadrantOf2Pi) {
+			case 1 :
+				// do nothing
+				break;
+			case 2 :
+				radians = MathFunctions.PI - radians;
+				break;
+			case 3 :
+				radians = radians - (3/2 * MathFunctions.PI) ;
+				break;
+			case 4 :
+				radians = 2 * MathFunctions.PI - radians;
+				break;
+			default :
+				break;
+		}
+		
+		// This is the Taylor expansion of sin(x)
+		for(int i = 0; i < accuracy; ++i) {
+			posNeg = -posNeg; // flip sign of this, ie. (-1)^(i)
+			result += posNeg * ( MathFunctions.intPower(radians, 2*i + 1) / 
+					MathFunctions.factorial(2*i+1));
+		}
+		
+		// The 3rd and 4th quadrants of the 0 to 2Pi curve lie under the x-axis -> flip sign
+		if(quadrantOf2Pi == 3 || quadrantOf2Pi == 4 )
+			result = -result;
+		
+		return result;
+	}
+	
+	/**
+	 * This gives cosecant(x) where x is in radians.
+	 * @param radians: double
+	 * @return secant(x) : double
+	 */
+	public static double csc(double radians) {
+		return 1 / cos(radians);
+	}
+	
+	/**
+	 * This gives tangent(x) where x is in radians.
+	 * @param radians: double
+	 * @return secant(x) : double
+	 */
+	public static double tan(double radians) {
+		return sin(radians) / cos(radians);
+	}
+	
+	/**
+	 * This gives cotangent(x) where x is in radians.
+	 * @param radians: double
+	 * @return secant(x) : double
+	 */
+	public static double cot(double radians) {
+		return cos(radians) / sin(radians);
+	}
+	
+	/**
+	 * So you give this method a radian value and it will convert it into the equivalent radian
+	 * value between 0 and 2Pi. It will return what quarter of this your angle is in.
+	 * ie. 15 radian is equivalent to 2.43 radians for sin/cos trig functions since they are
+	 * periodic on 2Pi. 2.43 falls in the the 2nd quadrant since it is greater than pi/2 but smaller
+	 * than pi.
+	 * @param radians : double
+	 * @return equivalent quadrant of 2Pi: int
+	 * @throws SHTFException
+	 */
+	private static int whatQuadrantOf2Pi(double radians) throws SHTFException {
+		
+		final double PI_OVER_2 = MathFunctions.PI / 2; // saved because used 4 times
+		
+		radians = mapRadiansToBetween0and2Pi(radians); // first step is to bring the radians down to 2 Pi.
+		
+		// This will tell you what quadrant of 2Pi your radians angles are equivalent to.
+		if(radians >= 0 && radians <= PI_OVER_2)
+			return 1;
+		if(radians > PI_OVER_2 && radians <= MathFunctions.PI)
+			return 2;
+		if(radians > MathFunctions.PI && radians <= 3*PI_OVER_2)
+			return 3;
+		if(radians > 3*PI_OVER_2 && radians <= 2*MathFunctions.PI)
+			return 4;
+		
+		// Should never happen as first step maps to 0 to 2Pi
+		throw new SHTFException("Something went wrong, please try again");
+	}
+	
+	/**
+	 * Given an angle in radians, this will return the equivalent angle mapped between 0 and 2*Pi
+	 * @param radians: double
+	 * @return radians mapped between 0 and 2Pi: double
+	 */
+	private static double mapRadiansToBetween0and2Pi(double radians) {
+		return radians = MathFunctions.abs(radians % (2 * MathFunctions.PI));
+	}
+	
 }
