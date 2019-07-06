08710597ec90174526a8d93e4e5fb5009bb35f6b
Daniel Thibault-Shea <daniel.thibaultshea@gmail.com>
2019-07-05 20:20:25 -0400

    Added SyntaxErrorException
    Thrown when the user's input expression String is not valid.

---
+++ COMP 354 - Calculator/SyntaxErrorException.java
@@ -0,0 +1,19 @@
+
+
+public class SyntaxErrorException extends ArithmeticException
+{
+	/**
+	 * Default constructor.
+	 */
+	public SyntaxErrorException() {
+		super("Syntax error");
+	}
+	
+	/**
+	 * Constructor that lets you set the message.
+	 * @param message:String
+	 */
+	public SyntaxErrorException(String message) {
+		super(message);
+	}
+}
