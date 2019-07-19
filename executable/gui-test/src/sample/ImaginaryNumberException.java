60d6e4c15951d9b371e360454f1f4bd4207dd696
juscas <justcastonguay@gmail.com>
2019-07-19 01:29:07 -0400

    added some stuff to executable folder

---
+++ executable/gui-test/src/sample/ImaginaryNumberException.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class ImaginaryNumberException extends ArithmeticException {
+
+    /**
+     * Default constructor.
+     */
+    public ImaginaryNumberException() {
+        super("Value is an imaginary number");
+    }
+
+    /**
+     * Constructor that lets you set the message.
+     * @param message:String
+     */
+    public ImaginaryNumberException(String message) {
+        super(message);
+    }
+
+}
